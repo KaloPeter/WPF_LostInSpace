27e71fa81fde8f6ef48799517e7a14dbc44a2e8c
kalopeter8 <kalopeter8@stud.uni-obuda.hu>
2023-04-20 14:22:03 +0200

    PlayArea setup

---
+++ WPF_LostInSpace/GameLogic/Logic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_LostInSpace.GameLogic
+{
+    public class Logic
+    {
+
+        private Size playArea;
+
+        public void SetUpPlayArea(Size playArea)
+        {
+           this.playArea = playArea;
+        }
+    }
+}
