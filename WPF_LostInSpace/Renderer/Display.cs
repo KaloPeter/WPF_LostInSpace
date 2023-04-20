32a01ac25a596ddd8b07cc93f7b9ad13b67852a0
kalopeter8 <kalopeter8@stud.uni-obuda.hu>
2023-04-20 14:23:37 +0200

    Rendered created(test cube)

---
+++ WPF_LostInSpace/Renderer/Display.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace WPF_LostInSpace.Renderer
+{
+    public class Display : FrameworkElement
+    {
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+
+
+            drawingContext.DrawRectangle(
+                Brushes.Red,
+                new Pen(Brushes.Black,2),
+                new Rect(100,100,20,20)
+                );
+
+        }
+    }
+}
