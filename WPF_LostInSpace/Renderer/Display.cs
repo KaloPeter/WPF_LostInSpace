136ed93e45417f37dcd731cf58c3dfa1732dbf22
kalopeter8 <kalopeter8@stud.uni-obuda.hu>
2023-04-20 16:26:38 +0200

    Render/display backgrounds from background list

--- WPF_LostInSpace/Renderer/Display.cs
+++ WPF_LostInSpace/Renderer/Display.cs
@@ -5,21 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+using WPF_LostInSpace.Interfaces;
 
 namespace WPF_LostInSpace.Renderer
 {
     public class Display : FrameworkElement
     {
+        private IGameLogic logic;
+
+
+        public void SetUpLogic(IGameLogic logic)
+        {
+            this.logic = logic;
+            logic.EventUpdateRender += (sender, eventArgs) => { this.InvalidateVisual(); };
+        }
+
         protected override void OnRender(DrawingContext drawingContext)
         {
             base.OnRender(drawingContext);
 
+            for (int i = 0; i < logic.GO_Backgrounds.Count; i++)
+            {
+                drawingContext.DrawRectangle(
+                    logic.GO_Backgrounds[i].BackgroundBrush,
+                    null,
+                    new Rect(logic.GO_Backgrounds[i].BackgroundPoint.X, logic.GO_Backgrounds[i].BackgroundPoint.Y,
+                    logic.GO_Backgrounds[i].BackgroundSize.Width, logic.GO_Backgrounds[i].BackgroundSize.Height)
+                    );
+            }
 
-            drawingContext.DrawRectangle(
-                Brushes.Red,
-                new Pen(Brushes.Black,2),
-                new Rect(100,100,20,20)
-                );
 
         }
     }
