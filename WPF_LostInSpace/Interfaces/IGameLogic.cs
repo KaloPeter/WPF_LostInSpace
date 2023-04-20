be6be4c9b111e58c4ae38caadded4171d2949c0d
kalopeter8 <kalopeter8@stud.uni-obuda.hu>
2023-04-20 16:26:09 +0200

    interface IGameLogic update

--- WPF_LostInSpace/Interfaces/IGameLogic.cs
+++ WPF_LostInSpace/Interfaces/IGameLogic.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPF_LostInSpace.GameObjects;
 
 namespace WPF_LostInSpace.Interfaces
 {
     public interface IGameLogic
     {
+      public List<GO_Background> GO_Backgrounds { get; set; }
+
+        event EventHandler EventUpdateRender;//to update the view, after new posotion was set.
     }
 }
