3aba304d220ed2c172d14605fa675eee9327ce32
kalopeter8 <kalopeter8@stud.uni-obuda.hu>
2023-04-20 16:26:57 +0200

    dispatcherTimer implemenatation

--- WPF_LostInSpace/MainWindow.xaml.cs
+++ WPF_LostInSpace/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using WPF_LostInSpace.GameLogic;
 using WPF_LostInSpace.HelperClasses;
 
@@ -23,19 +24,36 @@ namespace WPF_LostInSpace
     {
         private Logic logic;
 
+        private DispatcherTimer timer_backgroundMove;
+
+
         public MainWindow()
         {
+           
             InitializeComponent();
 
             logic = new Logic();
+            display.SetUpLogic(logic);
+            timer_backgroundMove = new DispatcherTimer();
+
+            timer_backgroundMove.Interval = TimeSpan.FromMilliseconds(0);
+            timer_backgroundMove.Tick += (sender, eventArgs) =>
+            {
+                logic.BackgroundMove();
+            };
+
 
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+         
             logic.SetUpPlayArea(new Size(grid.ActualWidth, grid.ActualHeight));
-            MessageBox.Show(new Size(grid.ActualWidth, grid.ActualHeight)+"");
+            logic.SetUpBackground();
+            
+            
 
+            timer_backgroundMove.Start();
 
         }
     }
