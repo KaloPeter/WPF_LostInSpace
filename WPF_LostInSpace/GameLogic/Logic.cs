016aa9b3dd34712556c097aec79bbc5c671da28c
Peter Kylo <kalopeter8@gmail.com>
2023-04-25 19:30:21 +0200

    Creating panels in Logic cstr, setting the size, point of panels

--- WPF_LostInSpace/GameLogic/Logic.cs
+++ WPF_LostInSpace/GameLogic/Logic.cs
@@ -23,16 +23,17 @@ namespace WPF_LostInSpace.GameLogic
         public Logic()
         {
             GO_Backgrounds = new List<GO_Background>();
-            GO_ControlPanels = new List<GO_ControlPanel>();
             GO_Background.LoadBackgrounds();
-            SetUpPanels();
-
-
-
             GO_Backgrounds.Add(new GO_Background());//empty space
             GO_Backgrounds.Add(new GO_Background());//empty space with earth and moon
 
 
+
+            GO_ControlPanels = new List<GO_ControlPanel>();
+            GO_ControlPanels.Add(new GO_ControlPanel("controlPanel_Health.png"));
+            GO_ControlPanels.Add(new GO_ControlPanel("controlPanel_Distance.png"));
+            GO_ControlPanels.Add(new GO_ControlPanel("controlPanel_Empty_L.png"));
+            GO_ControlPanels.Add(new GO_ControlPanel("controlPanel_Empty_R.png"));
         }
 
         public void SetUpPlayArea(Size playArea)
@@ -77,13 +78,32 @@ namespace WPF_LostInSpace.GameLogic
         }
         public void SetUpPanels()
         {
-            GO_ControlPanels.Add(new GO_ControlPanel("controlPanel_Health.png"));
-            GO_ControlPanels.Add(new GO_ControlPanel("controlPanel_Distance.png"));
-            GO_ControlPanels.Add(new GO_ControlPanel("controlPanel_Empty_L.png"));
-            GO_ControlPanels.Add(new GO_ControlPanel("controlPanel_Empty_R.png"));
+            //Panel order int collection: health, distance, empty_L, empty_R
 
-            GO_ControlPanels[0].ControlPanelPoint = new Point(0, 0);
-            GO_ControlPanels[0].ControlPanelSize = new Size(25, 25);
+            //health panel starts from top left corner, and the width of it is the start of the background, which is the axis X
+            GO_ControlPanels[1].ControlPanelPoint = new Point(0, 0);
+            //first one starts from 0,0, so the width of this panel is equelas with the axis X of background(bg starts here->this is how wide panel we need)
+            GO_ControlPanels[1].ControlPanelSize = new Size(GO_Backgrounds[0].BackgroundPoint.X, 200);
+
+
+            //second panel starts at the end of the background of x-axis(top right corner)
+            GO_ControlPanels[0].ControlPanelPoint = new Point(GO_Backgrounds[0].BackgroundPoint.X + GO_Backgrounds[0].BackgroundSize.Width, 0);
+            //second panel starts from the end of the bg(top right corner), and goes to the end of the window->
+            //windows width-from (0,0) to axis X - background size =>difference between right side of the windows and the right side of the background
+            GO_ControlPanels[0].ControlPanelSize = new Size(playArea.Width - GO_Backgrounds[0].BackgroundPoint.X - GO_Backgrounds[0].BackgroundSize.Width, 200);
+
+
+            GO_ControlPanels[2].ControlPanelPoint = new Point(0, GO_ControlPanels[0].ControlPanelSize.Height);
+            GO_ControlPanels[2].ControlPanelSize = new Size(GO_Backgrounds[0].BackgroundPoint.X, playArea.Height - GO_ControlPanels[0].ControlPanelSize.Height);
+
+            GO_ControlPanels[3].ControlPanelPoint = new Point(GO_ControlPanels[0].ControlPanelSize.Width + GO_Backgrounds[0].BackgroundSize.Width, GO_ControlPanels[1].ControlPanelSize.Height);
+            GO_ControlPanels[3].ControlPanelSize = new Size(playArea.Width - GO_ControlPanels[0].ControlPanelSize.Width - GO_Backgrounds[0].BackgroundSize.Width, playArea.Height - GO_ControlPanels[1].ControlPanelSize.Height);
+
+
+
+
+
+
 
             EventUpdateRender?.Invoke(this, null);
         }
