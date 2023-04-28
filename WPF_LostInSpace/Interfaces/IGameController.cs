5afe0a12499d520ed367f1b515a96e32222f7d6a
kalopeter8 <kalopeter8@gmail.com>
2023-04-28 12:32:22 +0200

    MovePlayer method implemented

--- WPF_LostInSpace/Interfaces/IGameController.cs
+++ WPF_LostInSpace/Interfaces/IGameController.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPF_LostInSpace.GameObjects;
 
 namespace WPF_LostInSpace.Interfaces
 {
     public interface IGameController
     {
-
+        void MovePlayer(PlayerController pc);
     }
 }
