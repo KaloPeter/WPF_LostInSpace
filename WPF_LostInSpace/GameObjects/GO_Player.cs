c45d8ae0fda768e31de27e8a48aaaa062c1624b8
py-snake <py-snake@gmx.com>
2023-04-28 22:31:35 +0200

    Astronaut background brush load to static

--- WPF_LostInSpace/GameObjects/GO_Player.cs
+++ WPF_LostInSpace/GameObjects/GO_Player.cs
@@ -8,21 +8,30 @@ namespace WPF_LostInSpace.GameObjects
 {
     public class GO_Player
     {
+        private Brush playerBrush;
+        private static Brush playerBrushLeft;
+        private static Brush playerBrushRight;
+
         public string Name { get; set; }
 
         public Point PlayerPoint { get; set; }
         public Size PlayerSize { get; set; }
+
         public int Money { get; set; }
         public int Speed { get; set; } = 2;
-
         public int Health { get; set; }
         public double Distance { get; set; }
 
-        private Brush playerBrush;
+        public Brush PlayerBrush { get { return playerBrush; } }
+        public Brush PlayerBrushLeft { get { return playerBrushLeft; } }
+        public Brush PlayerBrushRight { get { return playerBrushRight; } }
 
         public GO_Player()
         {
-            playerBrush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Astronaut", "astronaut_L.png"), UriKind.RelativeOrAbsolute)));
+            //playerBrush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Astronaut", "astronaut_L.png"), UriKind.RelativeOrAbsolute)));
+            playerBrushLeft = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Astronaut", "astronaut_L.png"), UriKind.RelativeOrAbsolute)));
+            playerBrushRight = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Astronaut", "astronaut_R.png"), UriKind.RelativeOrAbsolute)));
+            playerBrush = playerBrushLeft;
         }
 
         public void Move(PlayerController pc)
@@ -45,29 +54,27 @@ namespace WPF_LostInSpace.GameObjects
             this.PlayerPoint = newPoint;
         }
 
-        public Brush PlayerBrushLeft
-        {
-            get
-            {
-                Uri uri = new Uri(Path.Combine("Images", "Astronaut", "astronaut_L.png"), UriKind.RelativeOrAbsolute);
-                BitmapImage bitmImg = new BitmapImage(uri);
+        //public Brush PlayerBrushLeft
+        //{
+        //    get
+        //    {
+        //        Uri uri = new Uri(Path.Combine("Images", "Astronaut", "astronaut_L.png"), UriKind.RelativeOrAbsolute);
+        //        BitmapImage bitmImg = new BitmapImage(uri);
 
-                return new ImageBrush(bitmImg);
-            }
-        }
+        //        return new ImageBrush(bitmImg);
+        //    }
+        //}
 
-        public Brush PlayerBrushRight
-        {
-            get
-            {
-                Uri uri = new Uri(Path.Combine("Images", "Astronaut", "astronaut_R.png"), UriKind.RelativeOrAbsolute);
-                BitmapImage bitmImg = new BitmapImage(uri);
+        //public Brush PlayerBrushRight
+        //{
+        //    get
+        //    {
+        //        Uri uri = new Uri(Path.Combine("Images", "Astronaut", "astronaut_R.png"), UriKind.RelativeOrAbsolute);
+        //        BitmapImage bitmImg = new BitmapImage(uri);
 
-                return new ImageBrush(bitmImg);
-            }
-        }
-
-        public Brush PlayerBrush { get { return playerBrush; } }
+        //        return new ImageBrush(bitmImg);
+        //    }
+        //}
 
     }
 }
