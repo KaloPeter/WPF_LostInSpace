a83cf341ce3009c4ad25c16215bd415a905ebb67
kalopeter8 <kalopeter8@gmail.com>
2023-04-30 10:40:35 +0200

    Displaying Health,distance, collected money in window

--- WPF_LostInSpace/Renderer/Display.cs
+++ WPF_LostInSpace/Renderer/Display.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+using WPF_LostInSpace.GameObjects;
 using WPF_LostInSpace.Interfaces;
 
 namespace WPF_LostInSpace.Renderer
@@ -48,7 +49,7 @@ namespace WPF_LostInSpace.Renderer
             {
                 drawingContext.DrawRectangle(
                 logic.GO_Items[i].ItemBrush,
-                new Pen(Brushes.Red,2),
+                new Pen(Brushes.Red, 2),
                 new Rect(logic.GO_Items[i].ItemPoint.X, logic.GO_Items[i].ItemPoint.Y,
                 logic.GO_Items[i].ItemSize.Width, logic.GO_Items[i].ItemSize.Height)
                 );
@@ -59,7 +60,7 @@ namespace WPF_LostInSpace.Renderer
             {
                 drawingContext.DrawRectangle(
                     logic.GO_Lasers[i].LaserBrush,
-                    new Pen(Brushes.Yellow,2),
+                    new Pen(Brushes.Yellow, 2),
                     new Rect(logic.GO_Lasers[i].LaserPoint.X, logic.GO_Lasers[i].LaserPoint.Y,
                     logic.GO_Lasers[i].LaserSize.Width, logic.GO_Lasers[i].LaserSize.Height)
                     );
@@ -79,6 +80,45 @@ namespace WPF_LostInSpace.Renderer
                     logic.GO_ControlPanels[i].ControlPanelSize.Width, logic.GO_ControlPanels[i].ControlPanelSize.Height)
                     );
             }
+
+            FormattedText ft_distance = new FormattedText($"Distance: {logic.GO_Player.Distance} km", System.Globalization.CultureInfo.CurrentCulture,
+              FlowDirection.LeftToRight, new Typeface(new FontFamily("Arial"), FontStyles.Normal,
+              FontWeights.Normal, FontStretches.Normal
+              ), 20, Brushes.Yellow, 10);
+
+
+            drawingContext.DrawText(ft_distance, new Point(logic.GO_ControlPanels[1].ControlPanelPoint.X+25, 120));
+
+
+
+
+            FormattedText ft_health = new FormattedText(logic.GO_Player.Health + "+", System.Globalization.CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight, new Typeface(new FontFamily("Arial"), FontStyles.Normal,
+                FontWeights.Normal, FontStretches.Normal
+                ), 30, Brushes.Red, 10);
+
+
+            drawingContext.DrawText(ft_health, new Point(logic.GO_ControlPanels[0].ControlPanelPoint.X + 35, 30));
+
+
+
+            FormattedText ft_money = new FormattedText($"Money: {logic.GO_Player.Money}$", System.Globalization.CultureInfo.CurrentCulture,
+                FlowDirection.LeftToRight, new Typeface(new FontFamily("Arial"), FontStyles.Normal,
+                FontWeights.Normal, FontStretches.Normal
+                ), 30, Brushes.LightGreen, 10);
+
+
+            drawingContext.DrawText(ft_money, new Point(logic.GO_ControlPanels[3].ControlPanelPoint.X + 20, logic.GO_ControlPanels[3].ControlPanelPoint.Y + 25));
+
+
+
+
+            //drawingContext.DrawRectangle(
+            //    Brushes.Red,
+            //    new Pen(Brushes.Yellow, 2),
+            //    new Rect(logic.GO_ControlPanels[0].ControlPanelPoint.X, logic.GO_ControlPanels[0].ControlPanelPoint.Y,
+            //    50,50)
+            //    );
 
 
         }
