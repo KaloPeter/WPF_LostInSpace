ff294f88c1a0ed11557e3aed0796b937d1dbe753
kalopeter8 <kalopeter8@gmail.com>
2023-05-01 20:53:36 +0200

    Removing closing button(Red X), open/close instr. window

--- WPF_LostInSpace/InstructionWindow.xaml.cs
+++ WPF_LostInSpace/InstructionWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -9,6 +10,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -20,36 +22,49 @@ namespace WPF_LostInSpace
     /// </summary>
     public partial class InstructionWindow : Window
     {
-        MainWindow mainWindow;
-
         private static ImageBrush instruction_background = new ImageBrush(new BitmapImage(new Uri(System.IO.Path.Combine("Images", "Backgrounds", "InstructionsWindowBackground.jpg"), UriKind.RelativeOrAbsolute)));
+        private static string instructions = "Player movement:left and right arrow keys.\r\nAvoid asteroids and satellites.\r\nOxigen gives you health.\r\nTraver as far as you can.\r\nHave fun.";
+        private MainWindow mainWindow;
+
+        //********************************************************* Removes closing button and icon(left side)
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        [DllImport("user32.dll")]
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+        //*********************************************************
 
 
         public InstructionWindow(MainWindow mainWindow)
         {
-            InitializeComponent();
             this.mainWindow = mainWindow;
+            InitializeComponent();
+
         }
         private void bt_Close_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
-            mainWindow.Show();
+            mainWindow.EnableDisableMainMenuButtons(true);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             //lb_instructionsText.Content = File.ReadAllText(System.IO.Path.Combine("Others", "Instruction.txt"));//If we read from file, static variable to read from it only once
-            string instructions = "Player movement:left and right arrow keys.\r\nAvoid asteroids and satellites.\r\nOxigen gives you health.\r\nTraver as far as you can.\r\nHave fun.";
+
+            //********************************************************* Removes closing button and icon(left side)
+            var hwnd = new WindowInteropHelper(this).Handle;
+            SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+            //********************************************************* Removes closing button and icon(left side)
+
+
             lb_instructionsText.Content = instructions;
             GameInstructionsGrid.Background = instruction_background;
         }
 
-        private void Window_KeyDown(object sender, KeyEventArgs e)
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            //if (e.Key == Key.I)
-            //{
-            //    Window_Closed(sender, e);
-            //}
+            mainWindow.EnableDisableMainMenuButtons(true);
         }
     }
 }
