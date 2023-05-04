c969b7524ea6445aa207bdb0a9117fe8244c8750
py-snake <py-snake@gmx.com>
2023-05-04 21:55:55 +0200

    User class created

---
+++ WPF_LostInSpace/Userdata/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_LostInSpace.Userdata
+{
+    class User
+    {
+        public string Username { get; set; }
+        public int Money { get; set; }
+        public int BestDistance { get; set; }
+        public int TotalDistance { get; set; }
+
+        public double MusicVolume { get; set; }
+        public double EffectVolume { get; set; }
+
+    }
+}
