97d63b751d942f34b1da575b5baa889191e8e0c8
py-snake <py-snake@gmx.com>
2023-05-05 09:30:55 +0200

    Property added

--- WPF_LostInSpace/Userdata/User.cs
+++ WPF_LostInSpace/Userdata/User.cs
@@ -6,12 +6,13 @@ using System.Threading.Tasks;
 
 namespace WPF_LostInSpace.Userdata
 {
-    class User
+    public class User
     {
         public string Username { get; set; }
         public int Money { get; set; }
         public int BestDistance { get; set; }
         public int TotalDistance { get; set; }
+        public DateTime LastLogin { get; set; }
 
         public double MusicVolume { get; set; }
         public double EffectVolume { get; set; }
