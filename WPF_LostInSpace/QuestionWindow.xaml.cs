60f5e3eabb27b3edc169994f7a3f6c9953c8f1f4
kalopeter8 <kalopeter8@gmail.com>
2023-05-05 11:20:44 +0200

    Code Behind of QuestionWindow implmenetation

---
+++ WPF_LostInSpace/QuestionWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF_LostInSpace
+{
+    /// <summary>
+    /// Interaction logic for QuestionWindow.xaml
+    /// </summary>
+    public partial class QuestionWindow : Window
+    {
+        private static ImageBrush instruction_background = new ImageBrush(new BitmapImage(new Uri(System.IO.Path.Combine("Images", "Backgrounds", "InstructionsWindowBackground.jpg"), UriKind.RelativeOrAbsolute)));
+        private string question;
+        private string title;
+
+        public QuestionWindow(string question, string title)
+        {
+            this.question = question;
+            this.title = title;
+
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            GameQuestionsGrid.Background = instruction_background;
+            txQuestion.Text = question;
+        }
+
+        private void bt_Yes_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void bt_No_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        //We handle the right Mouse button, so user cant use it on the window
+        protected override void OnPreviewMouseRightButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseRightButtonDown(e);
+            e.Handled = true;
+        }
+
+        protected override void OnPreviewMouseRightButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseRightButtonUp(e);
+            e.Handled = true;
+        }
+
+    }
+}
