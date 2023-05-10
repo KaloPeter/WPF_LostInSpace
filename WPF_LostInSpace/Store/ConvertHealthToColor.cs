961c07cbfaa891d652c2b4fa827b38b5829abfa9
kalopeter8 <kalopeter8@gmail.com>
2023-05-10 22:34:13 +0200

    ColorConvert for Health/Speed Progressbar in Store

---
+++ WPF_LostInSpace/Store/ConvertHealthToColor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WPF_LostInSpace.Store
+{
+    public class ConvertHealthToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            int actVal = (int)value;
+
+            if (actVal == 100)
+            {
+                return Brushes.Red;
+            }
+            else if (actVal > 100 && actVal <= 250)
+            {
+                return Brushes.Green;
+            }
+            else
+            {
+                return Brushes.Purple;
+            }
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
---
+++ WPF_LostInSpace/Store/ConvertSpeedToColor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WPF_LostInSpace.Store
+{
+    public class ConvertSpeedToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            int actVal = (int)value;
+
+            if (actVal <= 2)
+            {
+                return Brushes.Red;
+            }
+            else if (actVal > 2 && actVal <= 4)
+            {
+                return Brushes.Yellow;
+            }
+            else
+            {
+                return Brushes.Green;
+            }
+
+
+
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
