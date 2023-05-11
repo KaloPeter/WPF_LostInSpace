fe7b01dddef70b5482e11a4a324b1ba1d1981a44
kalopeter8 <kalopeter8@stud.uni-obuda.hu>
2023-05-11 15:29:12 +0200

    prop modifications

--- WPF_LostInSpace/GameObjects/GO_Player.cs
+++ WPF_LostInSpace/GameObjects/GO_Player.cs
@@ -12,28 +12,26 @@ namespace WPF_LostInSpace.GameObjects
     public class GO_Player
     {
         private Brush playerBrush;
-        private Brush playerBrushLeft;
-        private Brush playerBrushRight;
 
         public string Name { get; set; }
         public Point PlayerPoint { get; set; }
         public Size PlayerSize { get; set; }
 
         public int Money { get; set; }
-        public int Speed { get; set; } = 2;
-        public int Health { get; set; } = 100;
+        public int Speed { get; set; }
+        public int Health { get; set; }
         public double Distance { get; set; }
 
-        public Brush PlayerBrush { get { return playerBrush; } }
-        public Brush PlayerBrushLeft { get { return playerBrushLeft; } }
-        public Brush PlayerBrushRight { get { return playerBrushRight; } }
+        public Brush PlayerBrush { get { return playerBrush; } set { playerBrush = value; } }
+        public Brush PlayerBrushLeft { get; set; }
+        public Brush PlayerBrushRight { get; set; }
 
         public GO_Player()
         {
             //playerBrush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Astronaut", "astronaut_L.png"), UriKind.RelativeOrAbsolute)));
-            playerBrushLeft = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Astronaut", "astronaut_1_L.png"), UriKind.RelativeOrAbsolute)));
-            playerBrushRight = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Astronaut", "astronaut_1_R.png"), UriKind.RelativeOrAbsolute)));
-            playerBrush = playerBrushLeft;
+            //    playerBrushLeft = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Astronaut", "astronaut_1_L.png"), UriKind.RelativeOrAbsolute)));
+            //    playerBrushRight = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "Astronaut", "astronaut_1_R.png"), UriKind.RelativeOrAbsolute)));
+            //    playerBrush = playerBrushLeft;
         }
 
         public void Move(PlayerController pc)
