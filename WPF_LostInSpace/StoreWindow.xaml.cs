f055f2a62402840e724832b4bbfa25fcfee2f35c
kalopeter8 <kalopeter8@stud.uni-obuda.hu>
2023-05-11 15:30:21 +0200

    Purchase/chosoe space suit

--- WPF_LostInSpace/StoreWindow.xaml.cs
+++ WPF_LostInSpace/StoreWindow.xaml.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 using WPF_LostInSpace.GameLogic;
 using WPF_LostInSpace.Store;
 
@@ -44,7 +46,14 @@ namespace WPF_LostInSpace
             //selectedItem
             selectedSuit = ((sender as ListBox).SelectedItem as SpaceSuit);
 
-
+            if (selectedSuit.Price == 0)
+            {
+                btPurchChoose.Content = "Choose";
+            }
+            else
+            {
+                btPurchChoose.Content = "Purchase";
+            }
 
             //Image myImage = new Image();
             //BitmapImage myImageSource = new BitmapImage();
@@ -61,8 +70,24 @@ namespace WPF_LostInSpace
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            //Active space suit load
-            lbMoney.Content = logic.GO_Player.Money;
+
+            selectedSuit = logic.SpaceSuits.Where(ss => ss.ID == logic.CurrentUser.LastSuitID).FirstOrDefault();
+            lbActiveSpaceSuitHealth.Content = selectedSuit.Health;
+            lbActiveSpaceSuitSpeed.Content = selectedSuit.Speed;
+
+            Image myImage = new Image();
+            BitmapImage myImageSource = new BitmapImage();
+            myImageSource.BeginInit();
+            myImageSource.UriSource = new Uri(selectedSuit.SpaceSuitResPath, UriKind.RelativeOrAbsolute);
+            myImageSource.EndInit();
+            myImage.Source = myImageSource;
+            lbActiveSpaceSuitImgRes.Content = myImage;
+            lbActiveSpaceSuitHealth.Content = "Health: " + selectedSuit.Health;
+            lbActiveSpaceSuitSpeed.Content = "Speed: " + selectedSuit.Speed;
+
+
+
+            lbMoney.Content = logic.CurrentUser.Money;
 
         }
 
@@ -80,23 +105,98 @@ namespace WPF_LostInSpace
         {
             if (selectedSuit != null)
             {
-                QuestionWindow qw = new QuestionWindow("Are you sure you want to purchase the choosen suit?", "Purchase confirm");
-
-                if (qw.ShowDialog() == true)
+                QuestionWindow qw;
+                if (selectedSuit.Price == 0)
                 {
 
+                    qw = new QuestionWindow("Are you sure you want to wear the choosen suit?", "Wear confirm");
 
-                    //minus player money
-                    //Add suit suit to players inventory by ID, remove suit price from store
-                    //make suit active?? 
-                    //owned property for spacesuit
-                    //activate button for owned suits
+                    if (qw.ShowDialog() == true)
+                    {
+                        Image myImage = new Image();
+                        BitmapImage myImageSource = new BitmapImage();
+                        myImageSource.BeginInit();
+                        myImageSource.UriSource = new Uri(selectedSuit.SpaceSuitResPath, UriKind.RelativeOrAbsolute);
+                        myImageSource.EndInit();
+                        myImage.Source = myImageSource;
+                        lbActiveSpaceSuitImgRes.Content = myImage;
+                        lbActiveSpaceSuitHealth.Content = "Health: " + selectedSuit.Health;
+                        lbActiveSpaceSuitSpeed.Content = "Speed: " + selectedSuit.Speed;
+
+                        logic.GO_Player.Health = selectedSuit.Health;
+                        logic.GO_Player.Speed = selectedSuit.Speed;
+
+                        logic.GO_Player.PlayerBrushLeft = selectedSuit.SpaceSuitBrush_L;
+                        logic.GO_Player.PlayerBrushRight = selectedSuit.SpaceSuitBrush_R;
+                        logic.GO_Player.PlayerBrush = selectedSuit.SpaceSuitBrush_R;
+
+                        logic.CurrentUser.LastSuitID = selectedSuit.ID;
+
+                        logic.SetUpPlayer();
+
+                        logic.SaveUsersToJson();
+                    }
+
+                }
+                else
+                {
+                    btPurchChoose.Content = "Purchase";
+                    qw = new QuestionWindow("Are you sure you want to purchase the choosen suit?", "Purchase confirm");
+
+                    if (qw.ShowDialog() == true)
+                    {
+
+                        if (logic.GO_Player.Money >= selectedSuit.Price)
+                        {
+                            logic.CurrentUser.Money -= selectedSuit.Price;
+                            logic.CurrentUser.PurchasedSpaceSuitIDX.Add(selectedSuit.ID);
+                            selectedSuit.Price = 0;
+
+
+                            Image myImage = new Image();
+                            BitmapImage myImageSource = new BitmapImage();
+                            myImageSource.BeginInit();
+                            myImageSource.UriSource = new Uri(selectedSuit.SpaceSuitResPath, UriKind.RelativeOrAbsolute);
+                            myImageSource.EndInit();
+                            myImage.Source = myImageSource;
+                            lbActiveSpaceSuitImgRes.Content = myImage;
+                            lbActiveSpaceSuitHealth.Content = "Health: " + selectedSuit.Health;
+                            lbActiveSpaceSuitSpeed.Content = "Speed: " + selectedSuit.Speed;
+
+                            lbMoney.Content = logic.GO_Player.Money;
+
+                            logic.GO_Player.Money = logic.CurrentUser.Money;
+                            logic.GO_Player.Health = selectedSuit.Health;
+                            logic.GO_Player.Speed = selectedSuit.Speed;
+
+                            logic.CurrentUser.LastSuitID = selectedSuit.ID;
+
+                            logic.SaveUsersToJson();
+
+                        }
+                        else
+                        {
+                            WarningWindow ww = new WarningWindow("Not enough money!", "Money problem");
+                            ww.Show();
+                        }
+
+
+
+
+
+                        // lbActiveSpaceSuitImgRes.Content = selectedSuit.SpaceSuitResPath;
+
+                        //minus player money
+                        //Add suit suit to players inventory by ID, remove suit price from store
+                        //make suit active?? 
+                        //owned property for spacesuit
+                        //activate button for owned suits
+                    }
                 }
 
-            }
-            else
-            {
-                MessageBox.Show("Choose spaceSuit!");
+
+
+
             }
         }
     }
