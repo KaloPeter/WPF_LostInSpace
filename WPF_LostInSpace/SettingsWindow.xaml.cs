d026dd89a03458b359b1a31c92be8198b819de39
kalopeter8 <kalopeter8@gmail.com>
2023-05-15 22:39:17 +0200

    Settings window with basic implementations

---
+++ WPF_LostInSpace/SettingsWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using WPF_LostInSpace.GameLogic;
+
+namespace WPF_LostInSpace
+{
+    /// <summary>
+    /// Interaction logic for SettingsWindow.xaml
+    /// </summary>
+    public partial class SettingsWindow : Window
+    {
+        private MainWindow mw = null;
+        private Logic logic = null;
+
+        public SettingsWindow(MainWindow mw,Logic logic)
+        {
+            this.mw = mw;
+            this.logic = logic;
+            InitializeComponent();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            mw.EnableDisableMainMenuButtons(true);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
