1676131218830f8ecfc8b7c2ab642041944df9ea
kalopeter8 <kalopeter8@gmail.com>
2023-05-22 13:45:17 +0200

    Removed public word from props/methods

--- WPF_LostInSpace/Interfaces/IGameLogic.cs
+++ WPF_LostInSpace/Interfaces/IGameLogic.cs
@@ -1,27 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using WPF_LostInSpace.GameObjects;
-using WPF_LostInSpace.Userdata;
+
 
 namespace WPF_LostInSpace.Interfaces
 {
     public interface IGameLogic
     {
-        public List<GO_Background> GO_Backgrounds { get; set; }
-        public List<GO_ControlPanel> GO_ControlPanels { get; set; }
+        List<GO_Background> GO_Backgrounds { get; set; }
+        List<GO_ControlPanel> GO_ControlPanels { get; set; }
 
         event EventHandler EventUpdateRender;//to update the view, after new position was set
 
-        public List<GO_Item> GO_Items { get; set; }
+        List<GO_Item> GO_Items { get; set; }
 
-        public List<GO_Laser> GO_Lasers { get; set; }
+        List<GO_Laser> GO_Lasers { get; set; }
 
-        public GO_Player GO_Player { get; set; }
+        GO_Player GO_Player { get; set; }
 
-        public List<byte[]> Cooldown_RGB { get; set; }
+        List<byte[]> Cooldown_RGB { get; set; }
 
     }
 }
