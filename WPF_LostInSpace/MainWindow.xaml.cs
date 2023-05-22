a8f5ca91e62c37273d1f6068019b1641d19183f6
kalopeter8 <kalopeter8@gmail.com>
2023-05-22 18:51:46 +0200

    code reorder

--- WPF_LostInSpace/MainWindow.xaml.cs
+++ WPF_LostInSpace/MainWindow.xaml.cs
@@ -1,22 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Windows.Threading;
 using WPF_LostInSpace.GameController;
 using WPF_LostInSpace.GameLogic;
-using WPF_LostInSpace.HelperClasses;
+
 
 namespace WPF_LostInSpace
 {
@@ -340,12 +332,5 @@ namespace WPF_LostInSpace
             }
         }
 
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-
-
-            // Application.Current.Shutdown();
-
-        }
     }
 }
